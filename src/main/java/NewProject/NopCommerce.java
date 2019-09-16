387054a97a22437698c0a85fa58ed72047e828c5
ajitsariakarishma <karishmaajitsaria@gmail.com>
2019-09-16 23:27:17 +0100

    Initial commit

---
+++ src/main/java/NewProject/NopCommerce.java
@@ -0,0 +1,207 @@
+package NewProject;
+
+import javafx.scene.input.DataFormat;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import static okhttp3.internal.http.HttpDate.format;
+import static org.openqa.selenium.By.xpath;
+
+public class NopCommerce extends Utils {
+
+    LoadProperty props = new LoadProperty();
+
+
+
+
+    @BeforeMethod
+    public void setup (){
+        launchChromeDriver();
+        driver.get(props.getProperty("url"));
+
+    }
+
+    @Test
+    public void userShouldBeAbleToRegisterSuccesfully(){
+
+        //click on 'Register' button
+        clickElement(By.xpath("//a[@class='ico-register']"));
+
+        //enter firstname
+        enterText(By.id("FirstName"),props.getProperty("Firstname"));
+
+        //enter last name
+        enterText(By.xpath("//input[@name='LastName']"),props.getProperty("Lastname"));
+
+        //Eneterting DOB
+        handlingDropdownByVisibleText(By.xpath("//select[@name='DateOfBirthDay']"),"5");
+        handlingDropdownByValue(By.xpath("//select[@name='DateOfBirthMonth']"),"10");
+        handlingDropdownByIndex(By.xpath("//select[@name='DateOfBirthYear']"),80);
+
+        //enter email
+        enterText(By.name("Email"),props.getProperty("email_start")+randomDate()+props.getProperty("email_end"));
+
+
+        //enter password
+        enterText(By.id("Password"),props.getProperty("password"));
+
+        //enter confirm password
+        enterText(By.xpath("//input[@name=\'ConfirmPassword']"),props.getProperty("password"));
+
+        //click on register button
+        clickElement(By.xpath("//input[@type='submit' and @name='register-button']"));
+
+        //storing value of actual in a string variable
+        String actual_msg = getTextFromElement(By.className("result"));
+        String expected_msg = "Your registration completed";
+        System.out.println("Actual Message is : "+actual_msg);
+
+        //comparing actual message with expected message
+        Assert.assertEquals(actual_msg,expected_msg);
+
+    }
+
+    @Test
+    public void emailFriend()  {
+
+        userShouldBeAbleToRegisterSuccesfully();
+
+        //click on nop commerce icon for opening homepage
+        clickElement(By.xpath("//img[@alt='nopCommerce demo store']"));
+
+        //click on imac pro image
+        clickElement(By.xpath("//img[@title='Show details for Apple MacBook Pro 13-inch']"));
+
+        //click on email friend
+        clickElement(By.xpath("//input[@value='Email a friend']"));
+
+        //enter email id of friend
+        enterText(By.xpath("//input[@class='friend-email']"),props.getProperty("friend_email"));
+
+
+        //enter message
+        enterText(By.id("PersonalMessage"),props.getProperty("email_msg"));
+
+        //click on send email button
+        clickElement(By.xpath("//input[@value='Send email']"));
+
+        String expectedConfirmMessage="Your message has been sent.";
+        String actualmessage=getTextFromElement(By.xpath("//div[@class='result']"));
+        System.out.println("Acutal confirmation message is : "+actualmessage);
+        Assert.assertEquals(actualmessage,expectedConfirmMessage);//comparing actual result with expected.
+
+    }
+    @Test
+    public void userShouldBeAbleToNavigateToCameraandPhotoPage(){
+        //creating instance of action class for mouse hover.
+        movedriverToWebElement(By.xpath("//ul[@class='top-menu notmobile']//a[contains(text(),'Electronics')]"));
+
+        //selecting and clicking camera and photo subcategory.
+        clickElement(By.xpath("//ul[@class='top-menu notmobile']//a[contains(text(),'Camera & photo')]"));
+
+
+        //getting and storing actual display message.
+        String actual_msg = getTextFromElement(By.xpath("//h1"));
+
+        String expected_msg = "Camera & photo";
+        System.out.println("Actual tittle displayed is: "+actual_msg);
+
+        //asserting the validity.
+        Assert.assertEquals(actual_msg, expected_msg);
+    }
+    @Test
+    public void userShouldBeAbleToFilterJewelleryByPrice() {
+        String ans;
+
+        //Clicking on Jewellery link on the homepage.
+        clickElement(By.xpath(" //ul[@class='top-menu notmobile']//a[contains(text(),'Jewelry')]"));
+
+        //Clicking on the filter attribute of range $700-$3000
+        clickElement(By.cssSelector("a[href='//demo.nopcommerce.com/jewelry?price=700-3000']"));
+
+        //getting relevant text according to the filter selected.
+        String price = getTextFromElement(By.xpath("//span[@class='price actual-price']"));
+        System.out.println(price);
+
+
+        //eliminating junk characters and converting string to integer value.
+        price = price.substring(0, 6);
+        price = price.replaceAll("[^0-9]", "");
+        int x = Integer.parseInt(price);
+        //checking the filter function
+        if (x >= 700 && x <= 3000) {
+            ans = "PASSED";
+            System.out.println("Your test has " + ans);
+        } else {
+
+            ans = "FAILED";
+            System.out.println("Your test has " + ans);
+
+        }
+        //checking user is navigated to jewllery page
+        String pageTitle=getTextFromElement(By.xpath("//h1[contains(text(),'Jewelry')]"));
+        String actual_title="Jewelry";
+        System.out.println("you are navigated to :"+actual_title+" page." );
+        Assert.assertEquals(pageTitle,actual_title);
+    }
+    @Test
+    public void userShouldBeAbleToAddTheProductsToTheShoppingCart()  {
+
+        //clicking on books link
+        clickElement(By.xpath("//ul[@class='top-menu notmobile']//a[contains(text(),'Books')]"));
+
+        //Clicking on a book
+        clickElement(By.xpath("//img[@alt='Picture of Fahrenheit 451 by Ray Bradbury']"));
+
+        //adding a book to the cart
+        clickElement(By.cssSelector("#add-to-cart-button-37"));
+
+        //clicking on books link
+        clickElement(By.xpath("//span[contains(text(),'Books')]"));
+
+        //selecting another book to add to cart
+        clickElement(By.cssSelector("img[title$='Prejudice']"));
+
+        //adding to cart
+        clickElement(By.cssSelector("#add-to-cart-button-39"));
+
+        //instructing browser to wait
+        waitUntilElementLoadsAndIsClickable(By.cssSelector("img[title$='Prejudice']"),60);
+
+        //clicking on shopping cart label to view the products added
+        clickElement(By.xpath("//span[@class='cart-label']"));
+
+        movedriverToWebElement(By.xpath("//span[@class='cart-label']"));
+
+
+        //storing and getting string value in a variable
+        String qty = getTextFromElement(By.xpath("//span[@class='cart-qty']"));
+        System.out.println("actual qty ordered: "+qty);
+        String expected_qty="(2)";
+        Assert.assertEquals(qty, expected_qty);
+
+
+    }
+
+
+    @AfterMethod
+    public void teardownr(){
+
+        closeDriver();
+
+    }
+
+}
+
---
+++ src/main/java/NewProject/Utils.java
@@ -0,0 +1,192 @@
+package NewProject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.NoSuchElementException;
+import java.util.SimpleTimeZone;
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.By.xpath;
+
+public class Utils extends BasePage {
+    //enter text at location
+    public static void enterText(By by, String text) {
+
+        driver.findElement(by).sendKeys(text);
+
+    }
+
+    // get text from  location
+    public static String getTextFromElement(By by) {
+        return driver.findElement(by).getText();
+
+    }
+
+    // click on web element
+    public static void clickElement(By by) {
+        driver.findElement(by).click();
+
+    }
+
+    // wait for element to be visiable
+    public static void waitForElementVisible(By by, long time) {
+        WebDriverWait wait = new WebDriverWait(driver, time);
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(by));
+
+    }
+
+    //wait for element to clickable
+    public static void waitUntilElementLoadsAndIsClickable(By by, long time) {
+        WebDriverWait wait = new WebDriverWait(driver, time);
+        wait.until(ExpectedConditions.elementToBeClickable(by));
+    }
+
+    //wait for alert to display
+    public static void forAlterPresent(long time) {
+        WebDriverWait wait = new WebDriverWait(driver, time);
+        wait.until(ExpectedConditions.alertIsPresent());
+
+    }
+
+    //to generate unique number everytime it runs
+    public static String randomDate() {
+        SimpleDateFormat simpledateformat = new SimpleDateFormat("ddMMyyHHmmss");
+        return simpledateformat.format(new Date());
+
+
+    }
+
+    //mouse hover movement
+    public static void movedriverToWebElement(By by) {
+        Actions actions = new Actions(driver);
+
+        //storing location of dropdown in webelement variable
+        WebElement menuList = driver.findElement(by);
+        actions.moveToElement(menuList).perform();
+
+    }
+
+    //launching chrome driver with pre requisites
+    public static void launchChromeDriver() {
+        System.setProperty("webdriver.chrome.driver", "src\\main\\java\\Resources\\WebBrowser\\chromedriver.exe");
+
+        //open the browser
+        driver = new ChromeDriver();
+
+        //maximise the browser window screen
+        driver.manage().window().fullscreen();
+
+        //set implicity wait for driver object
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+    }
+
+    //Handling dropdown by visible text
+    public static void handlingDropdownByVisibleText(By by, String text) {
+        Select select = new Select(driver.findElement(by));
+        select.selectByVisibleText(text);
+
+    }
+
+    //handling dropdown by value
+    public static void handlingDropdownByValue(By by, String text) {
+        Select select = new Select(driver.findElement(by));
+        select.selectByValue(text);
+    }
+
+    //handling dropdown by index number
+    public static void handlingDropdownByIndex(By by, int index_num) {
+        Select select = new Select(driver.findElement(by));
+        select.selectByIndex(index_num);
+    }
+
+    public static void conditionalWait(By by, int time) {
+
+        WebDriverWait wait = new WebDriverWait(driver, time);
+        //wait.until(ExpectedConditions.)
+    }
+
+    public static void clearTextFromInputBoxArea(By by) {
+
+        driver.findElement(by).clear();
+    }
+
+    public static void clearAndEnterText(By by, String text) {
+
+        driver.findElement(by).clear();
+        driver.findElement(by).sendKeys(text);
+
+    }
+
+    public static void isElementVisible(By by) {
+        boolean status = driver.findElement(by).isDisplayed();
+        System.out.println("Status of display of the Web Element: " + status);//if false then throws error
+
+    }
+
+    public static void isElementEnabled(By by) {
+        boolean status = driver.findElement(by).isEnabled();
+        System.out.println("Status of Web Element enabled: " + status);//is boolean function returns false if not enabled
+
+    }
+
+    public static void isElementSelected(By by) {
+        boolean status = driver.findElement(by).isSelected();
+        System.out.println("Status of Web Element selection: " + status);
+
+    }
+
+    public static void gettingTextOfSelectedOptionFromDropdownByvisibleText(By by, String text) {
+        Select select = new Select(driver.findElement(by));
+        select.selectByVisibleText(text);
+        WebElement option = select.getFirstSelectedOption();
+        String selected_option = option.getText();
+        System.out.println("Option seleted from dropdown is :  "+selected_option);
+
+    }
+    public static void gettingTextOfSelectedOptionFromDropdownByValue(By by, String text) {
+        Select select = new Select(driver.findElement(by));
+        select.selectByValue(text);
+        WebElement option = select.getFirstSelectedOption();
+        String selected_option = option.getText();
+        System.out.println("Option seleted from dropdown is :  " + selected_option);
+    }
+
+
+    public static void gettingTextOfSelectedOptionFromDropdownByIndexNumber(By by, int index_num) {
+        Select select = new Select(driver.findElement(by));
+        select.selectByIndex(index_num);
+        WebElement option = select.getFirstSelectedOption();
+        String selected_option = option.getText();
+        System.out.println("Option seleted from dropdown is :  " + selected_option);
+    }
+
+    public static void gettingAttributeofWebElement(By by, String value){
+        WebElement ele = driver.findElement(by);
+        System.out.println("Attribute of the " +value+"  given is : "+ele.getAttribute(value));
+
+    }
+
+    public static void getCssPropertyOfWebElement(By by,String text){
+        WebElement ele = driver.findElement(by);
+        System.out.println("Css value of the  "+text+" is : "+ele.getCssValue(text));
+
+    }
+
+    public static void closeDriver(){
+     driver.quit();
+    }
+
+
+}
+
